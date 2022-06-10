bee06483e9ea4b9cbf1325493d542829006bd2a3
MUKILTEO\1710005 <Taytonwhite@hotmail.com>
2022-06-10 08:39:27 -0700

    the beginning of garfeuild

---
+++ Assets/GarfeildMove.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarfeildMove : MonoBehaviour
+{
+    public int f = -4;
+    float t = 0;
+    float moveTime = 4f;
+    Vector3 oldPosition;
+    Vector3 newPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(f);
+        
+        newPosition = new Vector3(-4 * 2.24f, -0.22f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && f > -4)
+        {
+            oldPosition = transform.position;
+            f--;
+            t = 0;
+            newPosition = new Vector3(f * 2.24f + 1.12f, 1, 0);
+        }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) && f < 4)
+            {
+                oldPosition = transform.position;
+                f++;
+                t = 0;
+            
+            
+                newPosition = new Vector3(f * 2.24f - 1.12f, 1, 0);
+            
+          
+
+
+            }
+        t += Time.deltaTime;
+
+        if (t > moveTime / 2)
+        {
+            
+            newPosition = new Vector3(f * 2.24f, -0.22f, 0);
+        }
+        Debug.Log(t / moveTime);
+
+         transform.position = Vector3.Lerp(oldPosition, newPosition, t / moveTime );
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            oldPosition = transform.position;
+            
+            t = 0;
+            newPosition = new Vector3(f, -1, 0);
+        }
+    }
+    
+    }
+
---
+++ Assets/HiddengarfeildSpin.shader
@@ -0,0 +1,57 @@
+﻿Shader "Hidden/garfieldSpin"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = "white" {}
+    }
+        SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+
+            float getPos(float pos, float offset) {
+                return (pos + offset) % 1;
+            }
+            fixed4 frag(v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv);
+            // just invert the colors
+            col.rgb = tex2D(_MainTex, float2(i.uv.x, getPos(i.uv.y, (floor(_Time.x * 1000) % 2 == 0 ? (_Time.x * 1.45) + 0.023935 : (_Time.x * 1.45)) * 20)));
+
+
+            return col;
+        }
+        ENDCG
+    }
+    }
+}
