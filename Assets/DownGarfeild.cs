9bdbec99a5830ae890cd12f30ae4fd76bd3177a8
MUKILTEO\1710005 <Taytonwhite@hotmail.com>
2022-06-13 08:56:51 -0700

    garfeild down no worky :(

---
+++ Assets/DownGarfeild.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownGarfeild : MonoBehaviour
+{ 
+
+         public int f = -4;
+float t = 0;
+float moveTime = .5f;
+Vector3 oldPosition;
+Vector3 newPosition;
+Vector3 finalPosition;
+public bool moveComplete;
+public string movePhase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) && moveComplete)
+        {
+            moveComplete = false;
+            movePhase = "I";
+            oldPosition = transform.position;
+          
+            t = 0;
+            newPosition = new Vector3(oldPosition.x, -2, 0);
+        }
+
+        t += Time.deltaTime;
+        if (t >= (moveTime / 2))
+        {
+            if (movePhase == "I")
+            {
+                oldPosition = transform.position;
+                movePhase = "II";
+                t = t - moveTime / 2;
+            }
+            newPosition = new Vector3(oldPosition.x, -2f, 0);
+        }
+
+
+        if (t > moveTime)
+
+        {
+            moveComplete = true;
+        }
+
+        Debug.Log(t / moveTime + " " + moveComplete + " " + movePhase);
+
+        transform.position = Vector3.Lerp(oldPosition, newPosition, t / moveTime);
+        oldPosition = transform.position;
+    }
+}
--- Assets/GarfeildMove.cs
+++ Assets/GarfeildMove.cs
@@ -6,61 +6,72 @@ public class GarfeildMove : MonoBehaviour
 {
     public int f = -4;
     float t = 0;
-    float moveTime = 4f;
+    float moveTime = .5f;
     Vector3 oldPosition;
     Vector3 newPosition;
-
+    Vector3 finalPosition;
+    public bool moveComplete;
+    public string movePhase;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(f);
-        
         newPosition = new Vector3(-4 * 2.24f, -0.22f, 0);
+        moveComplete = false;
     }
-
     // Update is called once per frame
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && f > -4)
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && f > -4 && moveComplete)
         {
+            moveComplete = false;
+            movePhase = "I";
             oldPosition = transform.position;
             f--;
             t = 0;
             newPosition = new Vector3(f * 2.24f + 1.12f, 1, 0);
         }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && f < 4 && moveComplete)
+        {
+            moveComplete = false;
+            movePhase = "I";
+            oldPosition = transform.position;
+            f++;
+            t = 0;
+            newPosition = new Vector3(f * 2.24f - 1.12f, 1, 0);
+        }
 
-            if (Input.GetKeyDown(KeyCode.RightArrow) && f < 4)
+        t += Time.deltaTime;
+        if (t >= (moveTime / 2))
+        {
+            if (movePhase == "I")
             {
                 oldPosition = transform.position;
-                f++;
-                t = 0;
-            
-            
-                newPosition = new Vector3(f * 2.24f - 1.12f, 1, 0);
-            
-          
-
-
+                movePhase = "II";
+                t = t - moveTime / 2;
             }
-        t += Time.deltaTime;
-
-        if (t > moveTime / 2)
-        {
-            
             newPosition = new Vector3(f * 2.24f, -0.22f, 0);
         }
-        Debug.Log(t / moveTime);
 
-         transform.position = Vector3.Lerp(oldPosition, newPosition, t / moveTime );
+        if (t > moveTime)
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moveComplete = true;
+        }
+
+        Debug.Log(t / moveTime + " " + moveComplete + " " + movePhase);
+
+        transform.position = Vector3.Lerp(oldPosition, newPosition, t / moveTime);
+        if (Input.GetKeyDown(KeyCode.DownArrow) && moveComplete)
         {
             oldPosition = transform.position;
-            
-            t = 0;
-            newPosition = new Vector3(f, -1, 0);
+            moveComplete = false;
+            newPosition = new Vector3(f * 2.24f, -2, 0);
         }
     }
-    
-    }
+  
 
+
+
+}
